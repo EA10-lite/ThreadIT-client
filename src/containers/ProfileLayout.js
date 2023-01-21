ea8dfbb2451366b3f2d65c27944c64371d766552
Emmanuel <emanuelanyigor@gmail.com>
2023-01-20 22:18:46 -0800

    removing alt tags from images

--- src/containers/ProfileLayout.js
+++ src/containers/ProfileLayout.js
@@ -11,10 +11,10 @@ function ProfileLayout() {
         <div className="app__profile-container">
             <div className="app__profile-container__header">
                 <div className="app__profile-coverphoto">
-                    <img src={cover_photo} alt="coverphoto" />
+                    <img src={cover_photo}  />
                 </div>
                 <div className="app__profile-info">
-                    <img src={avatar} alt="profile picture" />
+                    <img src={avatar} />
                     <div className="app__profile-info__names">
                         <h4> Chris Emmanuel </h4>
                         <p> chris.jr </p>
